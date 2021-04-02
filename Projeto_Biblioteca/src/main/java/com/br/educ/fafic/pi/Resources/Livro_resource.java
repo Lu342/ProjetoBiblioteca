3a25ebd4ae8e0d0b980efe8cce7eb226b79c2400
Lucas Freitas <66649872+Lu342@users.noreply.github.com>
2021-04-01 21:20:22 -0300

    Adicionado o método deletar pelo uuid os livros

--- Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/Resources/Livro_resource.java
+++ Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/Resources/Livro_resource.java
@@ -1,7 +1,11 @@
 package com.br.educ.fafic.pi.Resources;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +34,12 @@ public class Livro_resource {
 	@GetMapping("/get/{isbn}")
 	public ResponseEntity getLivroByIsbn(@PathVariable String isbn) {
 		return ResponseEntity.ok().body(livro_service.getLivroByIsbn(isbn));
+	}
+
+	@DeleteMapping("/delete/{uuid}")
+	public ResponseEntity deleteLivro(@PathVariable("uuid") UUID uuid) {
+		livro_service.deleteLivro(uuid);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 	/*
--- Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/Service/Livro_servico.java
+++ Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/Service/Livro_servico.java
@@ -1,6 +1,7 @@
 package com.br.educ.fafic.pi.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,15 +23,20 @@ public class Livro_servico {
 	public Livro saveLivro(Livro livro) {
 		return livro_repository.save(livro);
 	}
-	
+
 	public Livro getLivroByIsbn(String isbn) {
 		return livro_repository.findByIsbn(isbn);
 	}
-	
+
+	public void deleteLivro(UUID uuid) {
+		Livro livro = livro_repository.getOne(uuid);
+		livro_repository.delete(livro);
+	}
+
 	/*
 	 * public List<Livro> getAll(){ return livro_repository.findAll(); }
-	 */ 
-	
-	/*public DELETE*/
+	 */
+
+	/* public DELETE */
 
 }
