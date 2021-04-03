ed4132922f6a716f9c36ee3850d14047e8893b0e
Lucas Freitas <66649872+Lu342@users.noreply.github.com>
2021-04-03 14:05:09 -0300

    Movimentação atualizado

--- Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/domain/Movimentacao.java
+++ Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/domain/Movimentacao.java
@@ -1,6 +1,9 @@
 package com.br.educ.fafic.pi.domain;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,14 +37,43 @@ public class Movimentacao {
 
 	@OneToOne
 	private Usuarios usuario;
-
+	
 	private Calendar dataEmprestimo;
 	private Calendar dataDevolucao;
 
 	@OneToMany
 	private List<Livro> livrosAlocados;
 	private Double multa;
+	public Double getMulta() {
+		return multa;
+	}
+	public void setMulta(Double multa) {
+		this.multa = multa;
+	}
+	
+    private String getDatePC() {
+	DateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+	Date date = new Date();
+	return dateFormat.format(date);
+}
+    /* Comparar duas datas em dias.
+     * 
+    SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+    String inputString1 = dataEmprestimo;
+    String inputString2 = dataDevolucao;
 
-//	 multa di√°ria de 2,00 para cada dia (se tiver)
-//   Usar o rel√≥gio do computador como base.
+    try {
+        Date date1 = myFormat.parse(inputString1);
+        Date date2 = myFormat.parse(inputString2);
+        long diff = date2.getTime() - date1.getTime();
+        System.out.println ("Days: " + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+    } catch (ParseException e) {
+        e.printStackTrace();
+    }
+*/
+    
+    
+//	 multa di·ria de 2,00 para cada dia (se tiver)
+//   Usar o relÛgio do computador como base.
+	
 }
