cc8a2131860aed817055f0b871ba7028789817da
Kenny <allisson155@gmail.com>
2021-04-05 11:05:24 -0300

    Implementando Serializable em algumas entidades.
    Método save em movimentacao?

--- Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/Service/Movimentacao_servico.java
+++ Projeto_Biblioteca/src/main/java/com/br/educ/fafic/pi/Service/Movimentacao_servico.java
@@ -1,6 +1,7 @@
 package com.br.educ.fafic.pi.Service;
 
 import com.br.educ.fafic.pi.Repository.Movimentacao_repository;
+import com.br.educ.fafic.pi.domain.Movimentacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,10 @@ public class Movimentacao_servico {
 	@Autowired
 	public Movimentacao_servico(Movimentacao_repository movimentacao_repository) {
 		this.movimentacao_repository = movimentacao_repository;
+	}
+
+	public Movimentacao saveMovimentacao(Movimentacao movimentacao) {
+		return movimentacao_repository.save(movimentacao);
 	}
 
 }
